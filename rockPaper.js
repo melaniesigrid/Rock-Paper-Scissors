5b8fd0e0273f6e5441c60298263565ced8028801
Melanie Arellano <melaniesigrid@protonmail.com>
2021-08-13 12:29:19 -0400

    Almost finished

--- rockPaper.js
+++ rockPaper.js
@@ -1,5 +1,12 @@
 // Solving https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
-const computerPlay = () => { //Randomly returns 'Rock', 'Paper', or 'Scissors'
+//Randomly returns 'Rock', 'Paper', or 'Scissors'
+// @ts-check
+let computerScore = 0;
+let playerScore = 0;
+let roundWinner = "";
+
+
+const computerPlay = () => { 
   let choices = ['rock', 'paper', 'scissors'];
   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
   return computerChoice;
@@ -8,40 +15,57 @@ const computerPlay = () => { //Randomly returns 'Rock', 'Paper', or 'Scissors'
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
+    computerScore++;
+    playerSelection++;
     return "It's a tie";
   } else {
     switch (playerSelection) {
       case 'rock':
         switch (computerSelection) {
           case 'paper':
+            computerScore++;
             return "You Lose! Paper beats Rock";
           case 'scissors':
+            playerScore++;
             return "You Win! Rock beats Scissors";
         }
       case 'paper':
         switch (computerSelection) {
           case 'rock':
+            playerScore++;
             return "You Win! Paper beats Rock";
           case 'scissors':
-            return "You Lose. Scissors beats Paper"
+            computerScore++;
+            return "You Lose. Scissors beats Paper";
         }
       case 'scissors':
         switch(computerSelection) {
           case 'rock':
+            computerScore++;
             return "You Lose. Rock beats scissors";
           case 'paper':
-            return "You Win. Scissors beats Paper"
-        }
+            playerScore++;
+            return "You Win. Scissors beats Paper";
+        } 
       }
 
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-
 const game = () => { // Plays a 5 round game that keeps score and reports a winner or loser at the end.
   for (let i = 0; i < 5; i++) {
-    computerPlay();
+    let computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (playerScore > computerScore) {
+    console.log("You are the final WINNER!");
+  } else if (computerScore > playerScore) {
+    console.log("Sorry, you LOSE");
+  } else {
+    console.log("It's a TIE");
+  }
 }
+
+let playerSelection = window.prompt("Rock, Paper, Scissors?", "Rock");
+
+game();
